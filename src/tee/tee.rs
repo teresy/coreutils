9c810956868a71cb65f21846ee6a7f6968f14743
teresy <hi.teresy@gmail.com>
2018-11-06 17:40:29 -0500

    refactor: use shorthand fields

--- src/tee/tee.rs
+++ src/tee/tee.rs
@@ -93,7 +93,7 @@ fn tee(options: Options) -> Result<()> {
         .into_iter()
         .map(|file| open(file, options.append))
         .collect();
-    let output = &mut MultiWriter { writers: writers };
+    let output = &mut MultiWriter { writers };
     let input = &mut NamedReader {
         inner: Box::new(stdin()) as Box<Read>,
     };
